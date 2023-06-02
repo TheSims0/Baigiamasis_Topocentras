c147bcac16683089f55bca14df2544d6a6b85c49
TheSims0 <mahtynas@gmail.com>
2023-06-02 23:16:33 +0300

    Add TS03_Compare Products

---
+++ Framework/Pages/Topocentras/CompareProducts.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+namespace Framework.Pages.Topocentras
+{
+    public class CompareProducts
+    {
+        public static void Open()
+        {
+            Driver.OpenPage("https://www.topocentras.lt/");
+        }
+
+        public static void CloseCookies()
+        {
+            Common.Click(Locators.AddToCart.cookies);
+        }
+
+        public static void ClickOnProductPage()
+        {
+            Common.HoverOnElement(Locators.CompareProducts.zaidimuErdve);
+            Common.Click(Locators.CompareProducts.zaidimuMonitoriai);
+        }
+
+        public static void AddProductsToCompare()
+        {
+            Common.WaitUntilElementIsVisibleAndClickable(Locators.CompareProducts.addToCompare);
+            Common.ClickElementsInList(Locators.CompareProducts.addToCompare);
+        }
+
+        public static void GoToComparePage()
+        {
+            Common.WaitUntilElementIsVisibleAndClickable(Locators.CompareProducts.comparePage);
+            Common.Click(Locators.CompareProducts.comparePage);
+        }
+
+        public static List<string> GetElementText()
+        {
+            Common.WaitUntilElementIsVisibleAndClickable(Locators.CompareProducts.productNames);
+            return Common.GetElementsTextList(Locators.CompareProducts.productNames);
+        }
+    }
+}
---
+++ Tests/Topocentras/TS03_CompareProducts.cs
@@ -0,0 +1,19 @@
+﻿using Framework.Pages.Topocentras;
+using NUnit.Framework;
+using Tests.BaseTests;
+namespace Tests.Topocentras
+{
+    internal class TS03_CompareProducts : BaseTest
+    {
+        [Test]
+        public void TC01_CompareThreeProducts()
+        {
+            CompareProducts.Open();
+            CompareProducts.CloseCookies();
+            CompareProducts.ClickOnProductPage();
+            CompareProducts.AddProductsToCompare();
+            CompareProducts.GoToComparePage();
+            Assert.That(CompareProducts.GetElementText(), Is.Unique);
+        }
+    }
+}
