e8d1e4ec0b25f2b0a18b146983e81c03f61f93d4
TheSims0 <mahtynas@gmail.com>
2023-06-02 23:16:16 +0300

    Add TS02_AddProductsToCart

---
+++ Framework/Pages/Topocentras/AddToCart.cs
@@ -0,0 +1,40 @@
+﻿namespace Framework.Pages.Topocentras
+{
+    public class AddToCart
+    {
+        public static void Open()
+        {
+            Driver.OpenPage("https://www.topocentras.lt/");
+        }
+
+        public static void ClickOnProduct()
+        {
+            Common.HoverOnElement(Locators.AddToCart.butineTechnika);
+            Common.Click(Locators.AddToCart.kaitlentes);
+        }
+
+        public static void AddProductsToCart()
+        {
+            Common.WaitUntilElementIsVisibleAndClickable(Locators.AddToCart.addToCart);
+            Common.ScrollUntilElementIsClickable(Locators.AddToCart.addToCart);
+            Common.ClickElementsInListAndClosePopUp(Locators.AddToCart.addToCart, Locators.AddToCart.closePopUp);
+        }
+
+        public static void CloseCookies()
+        {
+            Common.Click(Locators.AddToCart.cookies);
+        }
+
+        public static void GoToCart()
+        {
+            Common.ScrollUntilElementIsClickable(Locators.AddToCart.goToCart);
+            Common.Click(Locators.AddToCart.goToCart);
+        }
+
+        public static string GetTotalProducts()
+        {
+            Common.WaitUntilElementIsVisibleAndClickable(Locators.AddToCart.totalProducts);
+            return Common.CountElements(Locators.AddToCart.totalProducts);
+        }
+    }
+}
---
+++ Tests/Topocentras/TS02_AddProductsToCart.cs
@@ -0,0 +1,21 @@
+﻿using Framework.Pages.Topocentras;
+using NUnit.Framework;
+using Tests.BaseTests;
+namespace Tests.Topocentras
+{
+    internal class TS02_AddProductsToCart : BaseTest
+    {
+        [Test]
+        public void TC01_AddProductsToCart()
+        {
+            AddToCart.Open();
+            AddToCart.CloseCookies();
+            AddToCart.ClickOnProduct();
+            AddToCart.AddProductsToCart();
+            AddToCart.GoToCart();
+            string actualResult = AddToCart.GetTotalProducts();
+            string expectedResult = "4";
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
