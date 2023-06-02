2ce50b366cbddc165fec639cc0cec81a83ca8131
TheSims0 <mahtynas@gmail.com>
2023-06-02 23:15:56 +0300

    Added TS01_Login

---
+++ Framework/Pages/Topocentras/TopocentrasHomePage.cs
@@ -0,0 +1,43 @@
+﻿namespace Framework.Pages.Topocentras
+{
+    public class TopocentrasHomePage
+    {
+
+        public static void Open()
+        {
+            Driver.OpenPage("https://www.topocentras.lt/");
+        }
+
+        public static void ClickPrisijungti()
+        {
+            Common.Click(Locators.Login.prisijungti);
+        }
+
+        public static void EnterEmail()
+        {
+            Common.SendKeys(Locators.Login.enterEmail, "baigiamasisms2023@gmail.com");
+        }
+
+        public static void EnterPassword()
+        {
+            Common.SendKeys(Locators.Login.enterPassword, "Nesakysiu123@");
+        }
+
+        public static void ClickPrisijungtiInLoginMenu()
+        {
+            Common.Click(Locators.Login.prisijungtiInLoginMenu);
+        }
+
+        public static void ClickManoPaskyra()
+        {
+            Common.WaitUntilElementIsVisibleAndClickable(Locators.Login.manoPaskyra);
+            Common.Click(Locators.Login.manoPaskyra);
+        }
+
+        public static string ReadUserName(string expectedText)
+        {
+            Common.WaitUntilElementIsVisibleAndContainsExpectedText(Locators.Login.readUserName, expectedText);
+            return Common.GetElementText(Locators.Login.readUserName);
+        }
+    }
+}
---
+++ Tests/Topocentras/TS01_Login.cs
@@ -0,0 +1,22 @@
+﻿using Framework.Pages.Topocentras;
+using NUnit.Framework;
+using Tests.BaseTests;
+namespace Tests.Topocentras
+{
+    internal class TS01_Login : BaseTest
+    {
+        [Test]
+        public void TC01_LoginUsingValidData()
+        {
+            TopocentrasHomePage.Open();
+            TopocentrasHomePage.ClickPrisijungti();
+            TopocentrasHomePage.EnterEmail();
+            TopocentrasHomePage.EnterPassword();
+            TopocentrasHomePage.ClickPrisijungtiInLoginMenu();
+            TopocentrasHomePage.ClickManoPaskyra();
+            string expectedResult = "Simonas";
+            string actualResult = TopocentrasHomePage.ReadUserName(expectedResult);
+            StringAssert.Contains(expectedResult, actualResult);
+        }
+    }
+}
